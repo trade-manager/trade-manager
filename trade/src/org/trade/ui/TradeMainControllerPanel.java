0b3d06fc1aeab1126ce3c34a4ac2fae0e916badf
Simon Allen <simon.allen@gmail.com>
2012-10-08 18:16:00 -0700

    Added header note

--- trade/src/org/trade/ui/TradeMainControllerPanel.java
+++ trade/src/org/trade/ui/TradeMainControllerPanel.java
@@ -96,6 +96,7 @@ import org.trade.ui.strategy.StrategyPanel;
 import org.trade.ui.tradingday.TradingdayPanel;
 
 /**
+ * The apps main contraoller.
  */
 public class TradeMainControllerPanel extends TabbedAppPanel implements
 		BrokerChangeListener, StrategyChangeListener {
