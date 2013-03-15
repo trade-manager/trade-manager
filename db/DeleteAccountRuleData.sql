c1a0e9aef82bbafe4b02c4ba792d538f171ccdb0
Simon Allen <simon.allen@gmail.com>
2013-03-15 08:37:09 -0700

    reset name on paper account

--- db/DeleteAccountRuleData.sql
+++ db/DeleteAccountRuleData.sql
@@ -3,7 +3,8 @@
 USE ${sql.database};
 
 DELETE FROM portfolioaccount WHERE idPortfolioAccount >='0';
-DELETE FROM account WHERE idAccount>='0';
-DELETE FROM rule  WHERE idRule>='0';
+DELETE FROM account WHERE idAccount >='0';
+DELETE FROM rule  WHERE idRule >='0';
+UPDATE portfolio  SET name = 'Paper' WHERE idPortfolio = 1;
 commit;
 
