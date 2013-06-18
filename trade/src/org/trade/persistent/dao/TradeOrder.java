31fbf05d7014c16a94aa974b63d674105ab3d1ed
Simon Allen <simon.allen@gmail.com>
2013-06-18 08:12:55 -0700

    Added order Status notes

--- trade/src/org/trade/persistent/dao/TradeOrder.java
+++ trade/src/org/trade/persistent/dao/TradeOrder.java
@@ -1043,6 +1043,39 @@ public class TradeOrder extends Aspect implements java.io.Serializable,
 	/**
 	 * Method getStatus.
 	 * 
+	 * The order status. Possible values include: 
+	 * 
+	 * · PendingSubmit - indicates
+	 * that you have transmitted the order, but have not yet received
+	 * confirmation that it has been accepted by the order destination. NOTE:
+	 * This order status is not sent by TWS and should be explicitly set by the
+	 * API developer when an order is submitted.
+	 * 
+	 * · PendingCancel - indicates that you have sent a request to cancel the
+	 * order but have not yet received cancel confirmation from the order
+	 * destination. At this point, your order is not confirmed canceled. You may
+	 * still receive an execution while your cancellation request is pending.
+	 * NOTE: This order status is not sent by TWS and should be explicitly set
+	 * by the API developer when an order is canceled.
+	 * 
+	 * · PreSubmitted - indicates that a simulated order type has been accepted
+	 * by the IB system and that this order has yet to be elected. The order is
+	 * held in the IB system until the election criteria are met. At that time
+	 * the order is transmitted to the order destination as specified .
+	 * 
+	 * · Submitted - indicates that your order has been accepted at the order
+	 * destination and is working.
+	 * 
+	 * · Cancelled - indicates that the balance of your order has been confirmed
+	 * canceled by the IB system. This could occur unexpectedly when IB or the
+	 * destination has rejected your order.
+	 * 
+	 * · Filled - indicates that the order has been completely filled.
+	 * 
+	 * · Inactive - indicates that the order has been accepted by the system
+	 * (simulated orders) or an exchange (native orders) but that currently the
+	 * order is inactive due to system, exchange or other issues.
+	 * 
 	 * @return String
 	 */
 	@Column(name = "status", length = 45)
