a4f72b2917194ba6f51719b85aa1914385c769a4
Simon Allen <simon.allen@gmail.com>
2013-10-22 12:42:29 -0700

    Update Java SE to 45

--- trademanager.bat
+++ trademanager.bat
@@ -12,7 +12,7 @@ rem Change this dir to be the location for J2SE edition
 rem ***************************************************
 
 @if defined JAVA_HOME goto javahomedefined
-SET JAVA_HOME=C:/Program Files/Java/jdk1.7.0_25
+SET JAVA_HOME=C:/Program Files/Java/jdk1.7.0_45
 :javahomedefined
 
 rem set the path to include the bin dir
