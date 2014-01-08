a86f2809ab5c096778771586105fe36093854074
Simon Allen <simon.allen@gmail.com>
2014-01-08 07:32:55 -0800

    update notes allornothing

--- strategies/org/trade/strategy/PosMgrAllOrNothingStrategy.java
+++ strategies/org/trade/strategy/PosMgrAllOrNothingStrategy.java
@@ -41,9 +41,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trade.broker.BrokerModel;
 import org.trade.core.util.TradingCalendar;
-import org.trade.core.valuetype.Money;
-import org.trade.dictionary.valuetype.Action;
-import org.trade.dictionary.valuetype.Side;
 import org.trade.strategy.data.CandleSeries;
 import org.trade.strategy.data.StrategyData;
 import org.trade.strategy.data.candle.CandleItem;
@@ -54,21 +51,14 @@ public class PosMgrAllOrNothingStrategy extends AbstractStrategyRule {
 
 	/**
 	 * 1/ If the open position is filled create a STP (transmit=false see 2/ )
-	 * and 1 Target (LMT) OCA order at 3R with 100% of the filled quantity. Use
+	 * and 1 Target (LMT) OCA order at xR with 100% of the filled quantity. Use
 	 * the open position fill quantity, price and stop price to determine the
 	 * target price. The STP order take an initial risk of 1R.
 	 * 
-	 * 2/ The STP order should not be transmitted unless the current bars Vwap
-	 * crosses the stop price. Note this will result in more than a 1R stop.
-	 * This help with quick tails below the 5min low.
-	 * 
-	 * 3/ Target/Stop prices should be round over/under whole/half numbers when
+	 * 2/ Target/Stop prices should be round over/under whole/half numbers when
 	 * ever they are calculated..
 	 * 
-	 * 4/ At 10:30 move the STP order to the average fill price of the filled
-	 * open order.
-	 * 
-	 * 5/ Close any open positions at 15:58.
+	 * 3/ Close any open positions at 15:58.
 	 * 
 	 */
 
@@ -145,67 +135,9 @@ public class PosMgrAllOrNothingStrategy extends AbstractStrategyRule {
 				_log.info("Open position submit Stop/Tgt orders Symbol: "
 						+ getSymbol() + " Time:" + startPeriod);
 				createStopAndTargetOrder(getOpenPositionOrder(), 1, 3, this
-						.getOpenPositionOrder().getFilledQuantity(), false);
-			}
-			/*
-			 * Manage the stop orders if the close/open of this bar breaks the
-			 * stop price transmit the stop order this allows for tails that
-			 * break the 5min low before 10:30am
-			 */
-
-			if (startPeriod.before(TradingCalendar.getSpecificTime(startPeriod,
-					10, 30))) {
-
-				if (Side.BOT.equals(getOpenTradePosition().getSide())) {
-					if (currentCandle.getVwap() < getOpenPositionOrder()
-							.getStopPrice().doubleValue()) {
-						Money stopPrice = addPennyAndRoundStop(
-								getOpenPositionOrder().getStopPrice()
-										.doubleValue(), getOpenTradePosition()
-										.getSide(), Action.SELL, 0.01);
-						moveStopOCAPrice(stopPrice, true);
-						_log.info("Move Stop to b.e. Strategy Mgr cancelled Symbol: "
-								+ getSymbol()
-								+ " Time:"
-								+ startPeriod
-								+ " Price: " + stopPrice);
-					}
-				} else {
-
-					if (currentCandle.getVwap() > getOpenPositionOrder()
-							.getStopPrice().doubleValue()) {
-						Money stopPrice = addPennyAndRoundStop(
-								getOpenPositionOrder().getStopPrice()
-										.doubleValue(), getOpenTradePosition()
-										.getSide(), Action.BUY, 0.01);
-						moveStopOCAPrice(stopPrice, true);
-						_log.info("Move Stop to b.e. Strategy Mgr cancelled Symbol: "
-								+ getSymbol()
-								+ " Time:"
-								+ startPeriod
-								+ " Price: " + stopPrice);
-					}
-				}
+						.getOpenPositionOrder().getFilledQuantity(), true);
 			}
 
-			/*
-			 * 10:30 Move stop order to b.e.
-			 */
-			if (startPeriod.equals(TradingCalendar.getSpecificTime(startPeriod,
-					10, 30))) {
-
-				_log.info("Rule move stop to b.e.. Symbol:" + getSymbol()
-						+ " Time: " + startPeriod);
-				String action = Action.SELL;
-				if (getOpenTradePosition().getSide().equals(Side.SLD)) {
-					action = Action.BUY;
-				}
-				moveStopOCAPrice(
-						addPennyAndRoundStop(getOpenPositionOrder()
-								.getAverageFilledPrice().doubleValue(),
-								getOpenTradePosition().getSide(), action, 0.01),
-						true);
-			}
 			/*
 			 * Close any opened positions with a market order at the end of the
 			 * day.
