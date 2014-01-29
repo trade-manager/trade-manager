5feab1344b8407cf3af170814d36cde10afd8bc2
Simon Allen <Simon Allen@PURCH.gateway.2wire.net>
2014-01-28 17:53:39 -0800

    remove upgrade script

--- db/Upgrade-2.1_08_to_09.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE tradeprod.contract 
-CHANGE COLUMN validOptionType optionType VARCHAR(1) NULL DEFAULT NULL AFTER validExchanges;
-
-ALTER TABLE tradeprod.contract 
-CHANGE COLUMN optionType optionType VARCHAR(1) NULL DEFAULT NULL AFTER minTick;
