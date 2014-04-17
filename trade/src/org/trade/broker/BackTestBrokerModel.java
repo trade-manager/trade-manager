20d4bfd563d25b531b4cf9948eb52f6faa810485
Simon Allen <Simon Allen@PURCH.gateway.2wire.net>
2014-04-16 19:14:23 -0700

    Update error msg

--- trade/src/org/trade/broker/BackTestBrokerModel.java
+++ trade/src/org/trade/broker/BackTestBrokerModel.java
@@ -1333,8 +1333,9 @@ public class BackTestBrokerModel extends AbstractBrokerModel implements
 		boolean valid = true;
 		String errorMsg = "Symbol: "
 				+ tradestrategy.getContract().getSymbol()
-				+ " Bar Size/Chart Days combination was not valid for Yahoo API, these values have been updated.\n Please validate and save.\n "
-				+ "Note Chart Days/BarSize combinations for IB TWS: " + "\n"
+				+ " Bar Size/Chart Days combination was not valid for Yahoo API, these values have been updated."
+				+ "\n" + "Please validate and save." + "\n"
+				+ "Note Chart Days/BarSize combinations for Yahoo: " + "\n"
 				+ "Chart Hist = 1 D, Bar Size >= 1min" + "\n"
 				+ "Chart Hist > 1 D to 1 M, Bar Size >= 5min" + "\n"
 				+ "Chart Hist > 1 M to 3 M, Bar Size = 1 day";
