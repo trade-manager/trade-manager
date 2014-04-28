45cf770d2622e7b14365796620eaf6cc44dd45f2
Simon Allen <Simon Allen@192.168.1.64>
2014-04-28 10:42:37 -0700

    Update model

--- db/upgradeDB-2.1_12_to_2.1_13.sql
+++ db/upgradeDB-2.1_12_to_2.1_13.sql
@@ -1,2 +1,6 @@
 ALTER TABLE `tradeprod`.`contract` 
 CHANGE COLUMN `tradingHours` `tradingHours` VARCHAR(100) NULL DEFAULT NULL ;
+
+ALTER TABLE `tradeprod`.`contract` 
+CHANGE COLUMN `localSymbol` `localSymbol` VARCHAR(20) NULL DEFAULT NULL ,
+CHANGE COLUMN `symbol` `symbol` VARCHAR(20) NOT NULL ;
