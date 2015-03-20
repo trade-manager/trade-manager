b31d884e55a0d2c8ef686c2ea9526e65e2eeb8f3
Simon Allen <simon.allen@gmail.com>
2015-03-20 08:52:37 -0700

    Change CURRENT_TIME() to NOW() on portfolio insert

--- db/TradeManagerData.sql
+++ db/TradeManagerData.sql
@@ -185,6 +185,6 @@ INSERT INTO entrylimit (idEntryLimit,startPrice,endPrice,limitAmount, percentOfP
 COMMIT;
 
 
-INSERT INTO portfolio (idPortfolio, name, alias, description, isDefault, lastUpdateDate, version) VALUES (1, 'Paper','Paper Account','Paper trading account', 1, CURRENT_TIME(), 0);
+INSERT INTO portfolio (idPortfolio, name, alias, description, isDefault, lastUpdateDate, version) VALUES (1, 'Paper','Paper Account','Paper trading account', 1, NOW(), 0);
 
 COMMIT;
